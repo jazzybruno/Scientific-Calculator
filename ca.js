f18d96d0d42ece1d9c0269eaffeabbcbab80cf4f
Jazzy Bruno <jazzybruno45@gmail.com>
2021-12-31 17:42:26 +0200

    More operations

---
+++ ca.js
@@ -0,0 +1,64 @@
+function sine(angle){
+    return Math.sin(angle * Math.PI/180);
+}
+
+function cosine(angle){
+    return Math.cos(angle);
+}
+
+function tangent(angle){
+    return Math.tan(angle);
+}
+
+function square(num1){
+    return num1 * num1;
+}
+
+function square(num1){
+    return Math.sqrt(num1);
+}
+
+function LN(num1){
+    return Math.log(num1);
+}
+
+function calculate(){
+
+    let cat = Number(document.getElementById("first").value)
+    let operation = document.getElementById("operation").value
+    let answer =  document.getElementById("answer")
+    
+    
+    switch(operation){
+        case "sin":
+          result = sine(cat)
+          break;
+
+          case "cos":
+              result = cosine(cat)
+              break; 
+
+              case "tan":
+                  result = tangent(cat)
+                  break;
+
+                  case "sq":
+                      result = square(cat)
+                      break;
+
+                      case "sqrt":
+                          result =  root(cat)
+                          break;
+
+                          case "ln":
+                              result = LN(cat)
+                              break;
+
+                              default :
+                              result - "Invalid input"
+                              break;
+    }
+    
+    answer.textContent = `${cat} ${operation} = ${result}`
+}
+
